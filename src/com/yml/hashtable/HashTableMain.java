5b9a284250354bf1ea1466ee276d38cd47391095
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-15 23:40:30 +0530

    Initialized project

---
+++ src/com/yml/hashtable/HashTableMain.java
@@ -0,0 +1,10 @@
+package com.yml.hashtable;
+
+public class HashTableMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to hashTable program");
+
+	}
+
+}
---
+++ src/com/yml/hashtable/Node.java
@@ -0,0 +1,40 @@
+package com.yml.hashtable;
+
+public class Node<K> implements INode<K> {
+	private K key;
+	private INode<K> next;
+	
+	public Node(K key) {
+		
+	}
+	@Override
+	public K getKey() {
+		return this.key;
+	}
+
+	@Override
+	public void setKey(K key) {
+		this.key = key;
+		
+	}
+
+	@Override
+	public INode<K> getNext() {
+		return next;
+	}
+
+	@Override
+	public void setNext(INode<K> next) {
+		this.next = next;
+		
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder =new StringBuilder();
+		builder.append("MyNode}"+"Key=").append(key).append("}");
+		if(next != null)
+			builder.append("->").append(next);
+		return builder.toString();
+	}
+	
+}	
